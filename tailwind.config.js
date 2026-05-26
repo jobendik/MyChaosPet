3d109a552839ffebe0b8f57ee5cd0e5806992723
Jo Bendik Westbye <73761444+jobendik@users.noreply.github.com>
2026-05-26 15:24:34 -0500

    Support prototype opacity utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      opacity: {
+        7: '0.07',
+        8: '0.08',
+        12: '0.12',
+        18: '0.18',
+        35: '0.35',
+        55: '0.55',
+        65: '0.65',
+        85: '0.85',
+      },
+    },
   },
   plugins: [],
 };
